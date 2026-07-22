00df2f22a2c8c6bb4bfce8d359960585847e00c2
MNihal7961 <mnihal7961@gmail.com>
2026-07-22 13:38:22 +0530

    feat(gateway): implement authentication proxy middleware and update service response

--- gateway/src/main.ts
+++ gateway/src/main.ts
@@ -2,9 +2,11 @@ import 'dotenv/config';
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { authProxy } from './middlewares/proxy';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use('/auth', authProxy);
   const port = process.env.PORT ?? 8000;
   await app.listen(port);
   Logger.log(`Gateway started on port ${port}`, 'Bootstrap');
