f3f218fdf1bb0d5971c5444bc43a1944e04d7b6c
MNihal7961 <mnihal7961@gmail.com>
2026-07-22 14:07:26 +0530

    feat(auth): configure custom logger for Nest application bootstrap

--- gateway/src/main.ts
+++ gateway/src/main.ts
@@ -3,9 +3,12 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { authProxy } from './middlewares/proxy';
+import { AppLogger } from './common/app-logger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: new AppLogger(),
+  });
   app.use('/auth', authProxy);
   const port = process.env.PORT ?? 8000;
   await app.listen(port);
